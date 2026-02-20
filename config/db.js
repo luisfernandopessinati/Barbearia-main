63e2d718b61e3679298718d4b937322fce3b7d4a
Luis Fernando <luis.fernando.pessinati@gmail.com>
2026-02-20 12:26:05 -0300

    fix: usa variáveis de ambiente no banco

--- config/db.js
+++ config/db.js
@@ -1,9 +1,32 @@
-const Sequelize = require('sequelize')
+/* const Sequelize = require('sequelize')
 const sequelize = new Sequelize('barbearia', 'root', '123@qwe', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
 })
+
+sequelize.authenticate().then(function () {
+    console.log("Conectado")
+}).catch(function (erro) {
+    console.log("Erro ao se conectar" + erro)
+}) 
+
+module.exports = sequelize;
+*/
+
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: false,
+    }
+)
 
 sequelize.authenticate().then(function () {
     console.log("Conectado")

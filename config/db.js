64e731a6d8d0961b99b7283c157ae06d19b12660
Luis Fernando <luis.fernando.pessinati@gmail.com>
2026-02-23 17:21:56 -0300

    voltando tudo

--- config/db.js
+++ config/db.js
@@ -17,21 +17,16 @@ module.exports = sequelize;
 /* */
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: false,
     }
-  }
-);
+)
 
 sequelize.authenticate().then(function () {
     console.log("Conectado")

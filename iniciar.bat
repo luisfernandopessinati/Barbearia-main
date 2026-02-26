e04f86ef3ffe7048dcf3da480c7a251a5371c4be
Luis Fernando <luis.fernando.pessinati@gmail.com>
2026-02-26 08:42:26 -0300

    Ajustes iniciar servidor

--- iniciar.bat
+++ iniciar.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 10 /nobreak
 C:\Users\Luis\AppData\Roaming\npm\pm2.cmd resurrect
+PowerShell -Command "Start-Process -FilePath cloudflared -ArgumentList 'tunnel --config C:\Users\Luis\.cloudflared\config.yml run' -WindowStyle Hidden"

597ff785717fa8da8d1aa2d8b19c7ee55f0f2bf0
Luis Fernando <luis.fernando.pessinati@gmail.com>
2026-03-21 16:18:32 -0300

    agendamentoroutes

--- routes/agendamentoRoutes.js
+++ routes/agendamentoRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware'); // ✅ importa
 const agendamentoController = require('../controllers/agendamentoController');
 
-router.get('/agendamentos', agendamentoController.listar);
+router.get('/agendamentos', authMiddleware, agendamentoController.listar); // ✅ aplica
 
 module.exports = router;
